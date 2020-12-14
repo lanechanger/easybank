0005b16f56aaad8509ea5ef53b3d4827aebf0aaf
lanechanger <jeremy_ng_86@live.ca>
2020-12-13 22:35:22 -0500

    If applied, this commit will finalize the mobile hamburger menu.

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,25 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function(){
     console.log("click hamburger");
 
     if(header.classList.contains("open")){ // close hamburger menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.add('fade-out');
-        overlay.classList.remove('fade-in');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        });
     }else{ // open hamburger menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
-    }
-
-    
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    } 
 });
